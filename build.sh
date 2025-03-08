67902483d2cc83fa020694ab001a3c49d20b33a9
Derixithy <derixithy@users.noreply.github.com>
2025-03-08 09:53:45 +0100

    Added DevPod to installed components

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@ set -ouex pipefail
 
 systemctl enable podman.socket
 
+# Setup DevPod
+dnf5 -y install libappindicator-gtk3
+curl -L -o devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64" && sudo install -c -m 0755 devpod /usr/local/bin && rm -f devpod
 
